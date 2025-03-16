912562e7ef4431099d79fcad968de83e944371c0
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-17 00:34:19 +0530

    Fixed Angular Material Related error

--- E-Commerce-Frontend/src/app/app.component.ts
+++ E-Commerce-Frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [HomeComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ E-Commerce-Frontend/src/app/navbar/nav-content/nav-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-content',
+  imports: [],
+  templateUrl: './nav-content.component.html',
+  styleUrl: './nav-content.component.css'
+})
+export class NavContentComponent {
+
+}
