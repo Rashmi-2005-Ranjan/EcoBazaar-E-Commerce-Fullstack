5e7520d32736cee95525617b47e19c9fc626d2bc
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-17 01:46:23 +0530

    Navbar Section Started

--- E-Commerce-Frontend/src/app/navbar/navbar.component.ts
+++ E-Commerce-Frontend/src/app/navbar/navbar.component.ts
@@ -1,11 +1,20 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  activeSection: string = '';
 
+  openNavbarContent(section: string) {
+    this.activeSection = section;
+  }
+  navigateTo(path:any){
+
+  }
 }
