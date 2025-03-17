55e3e22cd16946b67a0ca866668129c79907507d
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-17 18:51:39 +0530

    Navbar Content Implemented

--- E-Commerce-Frontend/src/app/navbar/navbar.component.ts
+++ E-Commerce-Frontend/src/app/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
-import { NgIf } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, MatIconModule,MatButtonModule,MatMenuModule,NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
@@ -15,6 +18,6 @@ export class NavbarComponent {
     this.activeSection = section;
   }
   navigateTo(path:any){
-
+    
   }
 }
