15ea578a66f69f5fb1d87b58d8cf583daa1f4701
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-17 21:14:10 +0530

    Starting Nav Content Compnent

--- E-Commerce-Frontend/src/app/navbar/nav-content/nav-content.component.ts
+++ E-Commerce-Frontend/src/app/navbar/nav-content/nav-content.component.ts
@@ -1,13 +1,21 @@
 import { NgFor } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { navigation } from './nav-content';
 
 @Component({
   selector: 'app-nav-content',
   imports: [NgFor],
   templateUrl: './nav-content.component.html',
-  styleUrl: './nav-content.component.css',
+  styleUrls: ['./nav-content.component.css'], // Corrected "styleUrls"
+  standalone: true, // Add this if using standalone components
 })
 export class NavContentComponent {
-  category: any;
+  category:any;
   @Input() selectedSection: any;
+
+  ngOnInit() {
+    this.category = navigation;
+    console.log(navigation)
+    console.log(this.selectedSection);
+  }
 }
