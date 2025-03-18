d3d1ec5487d02f8ca8932eb209090575184bbfd0
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-18 23:48:27 +0530

    Footer Component added

--- E-Commerce-Frontend/src/app/app.component.ts
+++ E-Commerce-Frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, NavbarComponent],
+  imports: [HomeComponent, NavbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
