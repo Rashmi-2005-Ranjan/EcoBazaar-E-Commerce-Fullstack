e979af5d22e0c50c20e8cae2dc62a105c8237b1f
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-23 18:44:06 +0530

    Configure All The Routes Requird For User Portal

--- E-Commerce-Frontend/src/app/app.component.ts
+++ E-Commerce-Frontend/src/app/app.component.ts
@@ -4,12 +4,13 @@ import { ShareModule } from './Modules/share/share.module';
 import { HomeComponent } from './Modules/feature/Components/home/home.component';
 import { NavbarComponent } from './Modules/share/Components/navbar/navbar.component';
 import { FooterComponent } from './Modules/share/Components/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent,FooterComponent,FeatureModule,ShareModule],
+  imports: [NavbarComponent,FooterComponent,FeatureModule,ShareModule,RouterOutlet],
   // imports: [HomeComponent, NavbarComponent,FooterComponent,FeatureModule,ShareModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- E-Commerce-Frontend/src/app/app.routes.ts
+++ E-Commerce-Frontend/src/app/app.routes.ts
@@ -1,3 +1,64 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./Modules/feature/Components/home/home.component').then(
+        (m) => m.HomeComponent
+      ),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./Modules/feature/Components/cart/cart.component').then(
+        (m) => m.CartComponent
+      ),
+  },
+  {
+    path: 'product-details/:id',
+    loadComponent: () =>
+      import(
+        './Modules/feature/Components/product-details/product-details.component'
+      ).then((m) => m.ProductDetailsComponent),
+  },
+  {
+    path: 'checkout',
+    loadComponent: () =>
+      import('./Modules/feature/Components/checkout/checkout.component').then(
+        (m) => m.CheckoutComponent
+      ),
+  },
+  {
+    path: 'checkout/payment/:id',
+    loadComponent: () =>
+      import('./Modules/feature/Components/payment/payment.component').then(
+        (m) => m.PaymentComponent
+      ),
+  },
+  {
+    path: 'payment-success',
+    loadComponent: () =>
+      import(
+        './Modules/feature/Components/payment-success/payment-success.component'
+      ).then((m) => m.PaymentSuccessComponent),
+  },
+  {
+    path: 'account/orders',
+    loadComponent: () =>
+      import('./Modules/feature/Components/order/order.component').then(
+        (m) => m.OrderComponent
+      ),
+  },
+  {
+    path:'order/:id',loadComponent:()=>import('./Modules/feature/Components/order-details/order-details.component').then(m=>m.OrderDetailsComponent)
+  },
+  //* Routing with parameters In Angular Lazy Loading
+  {
+    path: ':lavelOne/:lavelTwo/:lavelThree',
+    loadComponent: () =>
+      import('./Modules/feature/Components/products/products.component').then(
+        (m) => m.ProductsComponent
+      ),
+  },
+];
