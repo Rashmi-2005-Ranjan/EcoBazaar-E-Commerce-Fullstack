48839ea43a0f0130418772dd044fb4f514253698
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-29 01:45:13 +0530

    implemented Product Card for each Of the Product

--- E-Commerce-Frontend/src/app/Modules/share/Components/product-card/product-card.component.ts
+++ E-Commerce-Frontend/src/app/Modules/share/Components/product-card/product-card.component.ts
@@ -1,12 +1,17 @@
 import { NgFor } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
   imports: [],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.css'
+  styleUrl: './product-card.component.css',
 })
 export class ProductCardComponent {
-  @Input() product:any;
+  @Input() product: any;
+  private router = inject(Router);
+  navigateToProductPage() {
+    this.router.navigate([`/product-details/5}`]);
+  }
 }
