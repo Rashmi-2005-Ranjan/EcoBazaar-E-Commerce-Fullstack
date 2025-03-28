15e0775d1f07763e93a52e396888497e0273ebd5
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-28 23:57:21 +0530

    Fix Design error In Product Review component From It's Parent Component

--- E-Commerce-Frontend/src/app/Modules/feature/Components/product-details/product-details.component.ts
+++ E-Commerce-Frontend/src/app/Modules/feature/Components/product-details/product-details.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatRadioModule} from '@angular/material/radio';
+import { ProductReviewCardComponent } from './product-review-card/product-review-card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [MatRadioModule,FormsModule,ProductReviewCardComponent,NgFor],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
+  reviews=[1,1,1,1,1,1];
 
+  selectedSize:any;
+  handleAddToCart(){
+    console.log("Selected Size: ", this.selectedSize);
+  }
 }
