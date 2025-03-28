a4d2467b766da9b7493a3fcc32123b657e9e35dd
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-28 23:56:12 +0530

    Creader Product review component For Every Product

---
+++ E-Commerce-Frontend/src/app/Modules/feature/Components/product-details/product-review-card/product-review-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-review-card',
+  imports: [],
+  templateUrl: './product-review-card.component.html',
+  styleUrl: './product-review-card.component.css'
+})
+export class ProductReviewCardComponent {
+}
