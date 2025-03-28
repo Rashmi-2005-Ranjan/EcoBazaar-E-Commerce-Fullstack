3709ca5a77aef796f822602440bdd83ffc4f747d
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-29 01:42:44 +0530

    Implemented Product Review Component

--- E-Commerce-Frontend/src/app/Modules/feature/Components/product-details/product-review-card/product-review-card.component.ts
+++ E-Commerce-Frontend/src/app/Modules/feature/Components/product-details/product-review-card/product-review-card.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { StarRatingComponent } from '../../../../share/Components/star-rating/star-rating.component';
 
 @Component({
   selector: 'app-product-review-card',
-  imports: [],
+  imports: [StarRatingComponent],
   templateUrl: './product-review-card.component.html',
   styleUrl: './product-review-card.component.css'
 })
