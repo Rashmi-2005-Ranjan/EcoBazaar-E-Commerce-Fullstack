509a19944d1b6bb2a66ce8e8c3b55fb2f9efe455
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-30 01:55:55 +0530

    Implemented Cart Component To Show Users Order Summary

--- E-Commerce-Frontend/src/app/Modules/feature/Components/cart/cart.component.ts
+++ E-Commerce-Frontend/src/app/Modules/feature/Components/cart/cart.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { CartItemComponent } from '../../../share/Components/cart-item/cart-item.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [NgIf,NgFor,CartItemComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+  cart=[1,1,1]
 
+  private router=inject(Router)
+
+  navigateToCheckOut(){
+    this.router.navigate(['checkout']);
+  }
 }
