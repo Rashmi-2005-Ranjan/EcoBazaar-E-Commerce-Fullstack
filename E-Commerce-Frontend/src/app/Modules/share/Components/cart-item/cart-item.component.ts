af60ba87c21ec3b90117af20c79928e18d2292a0
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-31 01:06:15 +0530

    Implemented Cart Item Component

--- E-Commerce-Frontend/src/app/Modules/share/Components/cart-item/cart-item.component.ts
+++ E-Commerce-Frontend/src/app/Modules/share/Components/cart-item/cart-item.component.ts
@@ -1,19 +1,20 @@
-import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-
+import { NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-cart-item',
-  imports: [MatButtonModule,MatIconModule],
+  imports: [MatButtonModule, MatIconModule, NgIf],
   templateUrl: './cart-item.component.html',
-  styleUrl: './cart-item.component.css'
+  styleUrl: './cart-item.component.css',
 })
 export class CartItemComponent {
-  updateCartItem(num:number){
+  @Input() showButton: any;
+  updateCartItem(num: number) {
     console.log(num);
   }
-  removeCartItem(){
-    console.log("Remove Cart Item")
+  removeCartItem() {
+    console.log('Remove Cart Item');
   }
 }
