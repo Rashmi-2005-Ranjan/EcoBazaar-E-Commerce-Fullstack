05a6a4704ad6e8eb1a9212b56ad94dcb56b7a1fa
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-31 00:57:24 +0530

    Implemented Payment Summary Page

--- E-Commerce-Frontend/src/app/Modules/feature/Components/payment/payment.component.ts
+++ E-Commerce-Frontend/src/app/Modules/feature/Components/payment/payment.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { AddressCardComponent } from '../../../share/Components/address-card/address-card.component';
+import { CartItemComponent } from '../../../share/Components/cart-item/cart-item.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-payment',
-  imports: [],
+  imports: [AddressCardComponent, CartItemComponent, NgIf, NgFor],
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.css'
+  styleUrl: './payment.component.css',
 })
 export class PaymentComponent {
-
+  products = [1,1,1];
 }
