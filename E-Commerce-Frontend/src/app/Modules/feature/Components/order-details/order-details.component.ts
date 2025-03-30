6222b77250c01b4526aa249e39d7a720b100759c
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-31 00:56:35 +0530

    Implemented Order Details Component

--- E-Commerce-Frontend/src/app/Modules/feature/Components/order-details/order-details.component.ts
+++ E-Commerce-Frontend/src/app/Modules/feature/Components/order-details/order-details.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { AddressCardComponent } from '../../../share/Components/address-card/address-card.component';
+import { OrderCardComponent } from '../order/order-card/order-card.component';
+import { NgFor } from '@angular/common';
+import { ShareModule } from '../../../share/share.module';
+import { OrderTrackerComponent } from '../../../share/Components/order-tracker/order-tracker.component';
 
 @Component({
   selector: 'app-order-details',
-  imports: [],
+  imports: [
+    AddressCardComponent,
+    OrderCardComponent,
+    NgFor,
+    OrderTrackerComponent,
+  ],
   templateUrl: './order-details.component.html',
-  styleUrl: './order-details.component.css'
+  styleUrl: './order-details.component.css',
 })
 export class OrderDetailsComponent {
+  orders = [1, 1, 1];
 
+  steps = [
+    { id: 0, title: 'PLACED', isCompleted: true },
+    { id: 1, title: 'CONFIRM', isCompleted: true },
+    { id: 2, title: 'SHIPPED', isCompleted: false },
+    { id: 3, title: 'DELIVERED', isCompleted: false },
+  ];
 }
