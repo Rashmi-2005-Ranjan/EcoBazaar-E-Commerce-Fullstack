b23fb6045bc91129c7930bc5ed1474b5b3b9d035
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-31 00:54:46 +0530

    Add Order Detils Component In Order Component

--- E-Commerce-Frontend/src/app/Modules/feature/Components/order/order.component.ts
+++ E-Commerce-Frontend/src/app/Modules/feature/Components/order/order.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { OrderCardComponent } from './order-card/order-card.component';
+import { NgFor } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-order',
-  imports: [],
+  imports: [MatCheckbox, OrderCardComponent, NgFor],
   templateUrl: './order.component.html',
-  styleUrl: './order.component.css'
+  styleUrl: './order.component.css',
 })
 export class OrderComponent {
+  private router = inject(Router);
+  orderFilter = [
+    { value: 'on_the_way', label: 'On The Way' },
+    { value: 'delivered', label: 'Delivered' },
+    { value: 'cancelled', label: 'Cancelled' },
+    { value: 'returned', label: 'Returned' },
+  ];
+  orders = [
+    [1, 1],
+    [1, 1, 1],
+  ];
 
+  navigateToOrderDetails = (id: number) => {
+    this.router.navigate([`order/${id}`]);
+  };
 }
