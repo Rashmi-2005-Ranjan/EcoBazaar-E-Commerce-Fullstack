0fd5bffa237dc51939d5cbdef262d4e794b52fa9
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-04-05 23:33:08 +0530

    Implemented Auth Response Model

--- E-Commerce-Backend/src/main/java/com/ecobazar/main/ecommerce/response/AuthResponse.java
+++ E-Commerce-Backend/src/main/java/com/ecobazar/main/ecommerce/response/AuthResponse.java
@@ -1,2 +1,30 @@
-package com.ecobazar.main.ecommerce.response;public class AuthResponse {
+package com.ecobazar.main.ecommerce.response;
+
+public class AuthResponse {
+    private String jwt;
+    private String message;
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public AuthResponse(String jwt , String message) {
+        this.jwt = jwt;
+        this.message = message;
+    }
+
+    public AuthResponse() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
