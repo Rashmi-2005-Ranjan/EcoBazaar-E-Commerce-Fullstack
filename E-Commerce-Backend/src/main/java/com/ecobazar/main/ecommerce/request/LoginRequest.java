30ce70abf2ad6f05353483a035b4dbe34cd2351c
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-04-05 23:32:17 +0530

    Implemented Login Model For Login Requests

--- E-Commerce-Backend/src/main/java/com/ecobazar/main/ecommerce/request/LoginRequest.java
+++ E-Commerce-Backend/src/main/java/com/ecobazar/main/ecommerce/request/LoginRequest.java
@@ -1,2 +1,25 @@
-package com.ecobazar.main.ecommerce.request;public class LoginRequest {
+package com.ecobazar.main.ecommerce.request;
+
+public class LoginRequest {
+    public String getEmail() {
+        return email;
+    }
+
+    public LoginRequest() {
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String email;
+    private String password;
 }
