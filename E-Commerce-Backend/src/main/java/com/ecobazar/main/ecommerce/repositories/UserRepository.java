1963f606124cbc4820852693223428706844f5b8
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-04-05 23:31:15 +0530

    Implemented User repository For Accessing JPA Repository Methods

--- E-Commerce-Backend/src/main/java/com/ecobazar/main/ecommerce/repositories/UserRepository.java
+++ E-Commerce-Backend/src/main/java/com/ecobazar/main/ecommerce/repositories/UserRepository.java
@@ -1,2 +1,10 @@
-package com.ecobazar.main.ecommerce.repositories;public interface UserRepository {
+package com.ecobazar.main.ecommerce.repositories;
+
+import com.ecobazar.main.ecommerce.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByEmail(String email);
 }
