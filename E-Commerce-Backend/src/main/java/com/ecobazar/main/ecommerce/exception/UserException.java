acca137f169293f3bbd7594aba627c5ef889c3cd
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-04-05 23:29:07 +0530

    Implemented Custom Exception For Showing User Exception

--- E-Commerce-Backend/src/main/java/com/ecobazar/main/ecommerce/exception/UserException.java
+++ E-Commerce-Backend/src/main/java/com/ecobazar/main/ecommerce/exception/UserException.java
@@ -1,2 +1,7 @@
-package com.ecobazar.main.ecommerce.exception;public class UserException {
+package com.ecobazar.main.ecommerce.exception;
+
+public class UserException extends Exception {
+    public UserException(String message) {
+        super ( message );
+    }
 }
