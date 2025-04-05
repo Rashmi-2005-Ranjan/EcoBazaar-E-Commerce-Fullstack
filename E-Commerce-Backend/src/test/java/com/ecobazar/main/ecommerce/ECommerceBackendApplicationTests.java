2d0de3bf1b56772343be3148ed5ffb0fbfee3bb3
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-04-05 23:38:28 +0530

    Added JWT Dependency In pom.xml

---
+++ E-Commerce-Backend/src/test/java/com/ecobazar/main/ecommerce/ECommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecobazar.main.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
