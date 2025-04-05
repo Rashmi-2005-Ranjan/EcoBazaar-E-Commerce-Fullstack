c0a1177781dd50a02facba651a123f094f10b433
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-04-05 23:34:38 +0530

    Implemented User Service Layer For Interaction With Repository Layer

--- E-Commerce-Backend/src/main/java/com/ecobazar/main/ecommerce/service/CustomUserServiceImplementation.java
+++ E-Commerce-Backend/src/main/java/com/ecobazar/main/ecommerce/service/CustomUserServiceImplementation.java
@@ -1,2 +1,28 @@
-package com.ecobazar.main.ecommerce.service;public class CustomUserServiceImplementation {
+package com.ecobazar.main.ecommerce.service;
+
+import com.ecobazar.main.ecommerce.model.User;
+import com.ecobazar.main.ecommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class CustomUserServiceImplementation implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail ( username );
+        if (user == null) {
+            throw new UsernameNotFoundException ( "User Not Found With email"+user.getEmail () );
+        }
+        List<GrantedAuthority> authorities=new ArrayList<> (  );
+        return new org.springframework.security.core.userdetails.User ( user.getEmail (), user.getPassword (), authorities );
+    }
 }
--- E-Commerce-Backend/src/main/java/com/ecobazar/main/ecommerce/service/UserService.java
+++ E-Commerce-Backend/src/main/java/com/ecobazar/main/ecommerce/service/UserService.java
@@ -1,2 +1,9 @@
-package com.ecobazar.main.ecommerce.service;public interface UserService {
+package com.ecobazar.main.ecommerce.service;
+
+import com.ecobazar.main.ecommerce.exception.UserException;
+import com.ecobazar.main.ecommerce.model.User;
+
+public interface UserService {
+    public User findUserById(Long id) throws UserException;
+    public User findUserProfileByJwt(String jwt) throws UserException;
 }
