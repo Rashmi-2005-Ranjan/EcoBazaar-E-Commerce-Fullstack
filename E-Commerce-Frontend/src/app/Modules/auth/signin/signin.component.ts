3148f9e0453c3df92b486a2e8c37d81d73976625
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-04-10 19:48:28 +0530

    Implement Sign In Component

---
+++ E-Commerce-Frontend/src/app/Modules/auth/signin/signin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-signin',
+  imports: [MatFormField,MatLabel],
+  templateUrl: './signin.component.html',
+  styleUrl: './signin.component.css'
+})
+export class SigninComponent {
+  
+}
