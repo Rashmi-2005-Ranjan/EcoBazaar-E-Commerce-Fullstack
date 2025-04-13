8a3097535b6bb9d7f932a50dafb0fc03965f9b39
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-04-13 23:47:01 +0530

    Make The Cart Item Component Dynamic

--- E-Commerce-Frontend/src/app/Modules/share/Components/cart-item/cart-item.component.ts
+++ E-Commerce-Frontend/src/app/Modules/share/Components/cart-item/cart-item.component.ts
@@ -2,6 +2,7 @@ import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { CartService } from '../../../../State/Cart/Cart Service/cart.service';
 
 @Component({
   selector: 'app-cart-item',
@@ -10,11 +11,22 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './cart-item.component.css',
 })
 export class CartItemComponent {
+  @Input() cartItem: any;
   @Input() showButton: any;
+
+  constructor(private cartService: CartService) {}
+
   updateCartItem(num: number) {
     console.log(num);
+    this.cartService.updateCartItem({
+      cartItemId: this.cartItem.id,
+      data: {
+        quantity: num + this.cartItem.quantity,
+      },
+    });
   }
   removeCartItem() {
     console.log('Remove Cart Item');
+    this.cartService.removeCartItem(this.cartItem.id);
   }
 }
